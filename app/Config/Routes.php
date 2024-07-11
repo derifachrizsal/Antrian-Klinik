bdc50bd309c71d4cbbe5a96b5dc3bbb817f3b9a5
derifachrizsal <deri.fachrizsal@widyatama.ac.id>
2024-07-12 01:26:18 +0700

    conflict

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,12 +9,19 @@ use CodeIgniter\Router\RouteCollection;
 // BackEnd
 $routes->get('/adm', 'Backend\DashboardController::index');
 $routes->get('/adm/pasien', 'Backend\PasienController::index');
+// Backend | dokter
 $routes->get('/adm/dokter', 'Backend\DokterController::index');
 $routes->add('/adm/dokter/add', 'Backend\DokterController::tambah');
 $routes->add('/adm/dokter/(:segment)/edit', 'Backend\DokterController::edit/$1');
 $routes->get('/adm/dokter/(:segment)/delete', 'Backend\DokterController::delete/$1');
 $routes->get('/adm/user', 'Backend\UserController::index');
 $routes->add('/adm/user/add', 'Backend\UserController::tambah');
+// Backend | pasien
+$routes->get('/adm/pasien', 'Backend\PasienController::index');
+$routes->add('/adm/pasien/add', 'Backend\PasienController::tambah');
+$routes->add('/adm/pasien/(:segment)/edit', 'Backend\PasienController::edit/$1');
+$routes->get('/adm/pasien/(:segment)/delete', 'Backend\PasienController::delete/$1');
+
 // FrontEnd
 $routes->get('/', 'Frontend\HomeController::index');
 $routes->get('/dokter', 'Frontend\DokterController::index');
